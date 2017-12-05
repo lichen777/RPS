ee814b40ea29170cc2c58204d2638f6a1c4f944a
lichen777 <lchen139@gmail.com>
2017-12-05 16:14:10 -0600

    initial upload

---
+++ assets/javascript/app.js
@@ -0,0 +1,97 @@
+const game = {
+  r : {
+    r : "tie",
+    p : "lose",
+    s : "win"
+  },
+  p : {
+    r : "win",
+    p : "tie",
+    s : "lose"
+  },
+  s : {
+    r : "lose",
+    p : "win",
+    s : "tie"
+  },
+  choices : ["r", "p", "s"],
+  choicesImg : ["assets/images/r.png", "assets/images/p.png", "assets/images/s.png"],
+
+  showChoices : function () {
+    $('game').empty();
+    for (var i = 0; i < this.choices.length; i++) {
+      var images = $('<img>').attr('src', this.choicesImg[i]).attr('data-rps',this.choices[i]);
+      $('#game').append(images);
+    }
+  },
+
+  start : function () {
+    game.showChoices();
+
+    $('<img>').click(function () {
+      var value = ($(this).attr("data-rps"));
+      console.log(value);
+
+    })
+  },
+
+}
+
+const initialwin = 0;
+const initiallose = 0;
+
+const player = {
+  name : "",
+  win : initialwin,
+  lose : initiallose
+}
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCaZegY5dEdnNRP_E-KPB1l9nCwAmlKp4Y",
+  authDomain: "rps-multi-18d77.firebaseapp.com",
+  databaseURL: "https://rps-multi-18d77.firebaseio.com",
+  projectId: "rps-multi-18d77",
+  storageBucket: "rps-multi-18d77.appspot.com",
+  messagingSenderId: "290724551069"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var connectionsRef = database.ref("/connections");
+var connectedRef = database.ref(".info/connected");
+
+connectedRef.on("value", function(snap) {
+  if (snap.val()) {
+    var con = connectionsRef.push(true);
+    con.onDisconnect().remove();
+  }
+});
+
+connectionsRef.on("value", function(snap) {
+  $("#watchers").text(snap.numChildren());
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+
+})
+
+
+
+
+
+
