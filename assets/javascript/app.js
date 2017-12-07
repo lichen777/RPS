ef50762dc20b119b647dd9801edd8c33db4e6b96
lichen777 <lchen139@gmail.com>
2017-12-07 08:52:31 -0600

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-const game = {
+const logic = {
   r : {
     r : "tie",
     p : "lose",
@@ -13,44 +13,169 @@ const game = {
     r : "lose",
     p : "win",
     s : "tie"
-  },
+  }
+} // Object: logic
+
+const panel = {
+
   choices : ["r", "p", "s"],
   choicesImg : ["assets/images/r.png", "assets/images/p.png", "assets/images/s.png"],
 
-  showChoices : function () {
-    $('game').empty();
+  showChoices : function (id) {
+    $(id).empty();
     for (var i = 0; i < this.choices.length; i++) {
-      var images = $('<img>').attr('src', this.choicesImg[i]).attr('data-rps',this.choices[i]);
-      $('#game').append(images);
+      var images = $('<img>').attr('src', this.choicesImg[i]).attr('data-rps',this.choices[i]).addClass("choices");
+      $(id).append(images);
     }
-  },
 
-  showStatus : function (player) {
-    var name = $('<div>').text(player.name);
-    var win = $('<div>').text("win: " + player.win);
-    var lose = $('<div>').text("lose: " + player.lose);
-    $('#player1status').append(name).append(win).append(lose);
-  }
-
-  start : function () {
-    game.showChoices();
-
-    $('<img>').click(function () {
+    $('.choices').click(function () {
       var value = ($(this).attr("data-rps"));
       console.log(value);
+      $(id).empty();
+      var image = $('<img>').attr('src', panel.choicesImg[(panel.choices.indexOf(value))]);
+      $(id).append(image);
+      $(id).append("<p>selected</p>");
+      
+      if (id === '#player1'){
+        player1.selected = value;
+      } else if (id === '#player2') {
+        player2.selected = value;
+      } 
+    });
+  },
 
+  playerLogin : function() {
+    $('#player1-btn').click(function (event) {
+      event.preventDefault();
+      var input = $("#player1-name").val().trim();
+      if(input){
+        console.log(input);
+        $('#player1-name').val("");
+        player1.name = input;
+        yourRole = player1.id;
+        panel.playerLogged();
+        database.ref("/player1").set({
+          name: player1.name,
+          win: player1.win,
+          lose: player1.lose
+        });
+      } 
+    })
+    $('#player2-btn').click(function (event) {
+      event.preventDefault();
+      var input = $("#player2-name").val().trim();
+      if(input){
+        $('#player2-name').val("");
+        player2.name = input;
+        yourRole = player2.id;
+        panel.playerLogged();
+        database.ref("/player2").set({
+          name: player2.name,
+          win: player2.win,
+          lose: player2.lose
+        });
+      }
     })
   },
 
+  playerLogged : function () {
+    $('#player1-login').hide();
+    $('#player2-login').hide();
+    if (player1.name !== "") {
+      var text = $('<div>').text("Waiting for Another Player to Join").addClass("waiting");
+      $('#player2-block').prepend(text);
+      panel.showStatus();
+      
+      
+    } else if (player2.name !== "") {
+      var text = $('<div>').text("Waiting for Another Player to Join").addClass("waiting");
+      $('#player1-block').prepend(text);
+      panel.showStatus();
+      
+    }
+
+  },
+
+  showStatus : function () {
+    if(player1.name !== ""){
+      $('#player1-login').hide();
+      $('#player1-status').empty();
+      var name = $('<div>').text(player1.name);
+      var win = $('<div>').text("win: " + player1.win);
+      var lose = $('<div>').text("lose: " + player1.lose);
+      $('#player1-status').append(name).append(win).append(lose);   
+    }
+    
+    if(player2.name !== ""){
+      $('#player2-login').hide();
+      $('#player2-status').empty();
+      name = $('<div>').text(player2.name);
+      win = $('<div>').text("win: " + player2.win);
+      lose = $('<div>').text("lose: " + player2.lose);
+      $('#player2-status').append(name).append(win).append(lose);
+    }
+  }
+  
+} // Object: panel
+
+const game = {
+
+  dataUpdate : function() {
+    database.ref("/player1").on("value", function(snapshot) {
+      if (snapshot.val().name){
+        player1.name = snapshot.val().name;
+        player1.win = snapshot.val().win;
+        player1.lose = snapshot.val().lose;
+        panel.showStatus();
+      }
+    })
+    database.ref("/player2").on("value", function(snapshot) {
+      if (snapshot.val().name){
+        player2.name = snapshot.val().name;
+        player2.win = snapshot.val().win;
+        player2.lose = snapshot.val().lose;
+        panel.showStatus();
+      }
+    })
+    panel.playerLogin();
+  },
+
+  start : function () {
+    game.dataUpdate();
+    //panel.playerLogin();
+    database.ref().on("value", function(snapshot) {
+      if(snapshot.child("player1").child("name").val() && snapshot.child("player2").child("name").val()){
+        console.log("yes");
+        $('.waiting').empty();
+        panel.showChoices(yourRole);
+      }
+    });
+
+
+    //panel.showStatus();
+
+  }
+
+} // Object: game
+
+const initialNum = 0;
+
+let yourRole = "";
+
+const player1 = {
+  name : "",
+  win : initialNum,
+  lose : initialNum,
+  id : "#player1",
+  selected : ""
 }
 
-const initialwin = 0;
-const initiallose = 0;
-
-const player = {
+const player2 = {
   name : "",
-  win : initialwin,
-  lose : initiallose
+  win : initialNum,
+  lose : initialNum,
+  id : "#player2",
+  selected : ""
 }
 
 // Initialize Firebase
@@ -81,24 +206,8 @@ connectionsRef.on("value", function(snap) {
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 $(document).ready(function() {
-
+  
+  game.start();
+  
 })
-
-
-
-
-
-
